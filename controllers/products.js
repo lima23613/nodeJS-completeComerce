e53406ec51c98bc202e22d545cbcbf249877a710
lima23613 <magnificolima@hotmail.com>
2019-01-13 23:43:02 +0000

    add data on file

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddproduct = (req,res,next) =>{
     res.render('add-product',
@@ -11,18 +11,21 @@ exports.getAddproduct = (req,res,next) =>{
 }
 
 exports.postAddproduct = (req,res,next) =>{
-    products.push({title:req.body.title})
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect('/');
 }
 
-exports.getShop = (req,res,next) => {
-    res.render('shop',
-        {prods: products, 
-        docTitle:'Shop',
-        path:'/admin/shop', 
-        hasProduct: products.length > 0,
-        shopActive: true,
-        productCSS: true}
-    );  
+exports.getShop = (req, res, next) => {
+    Product.fetchAll((products) => res.render('shop',
+        {
+            prods: products,
+            docTitle: 'Shop',
+            path: '/admin/shop',
+            hasProduct: products.length > 0,
+            shopActive: true,
+            productCSS: true
+        }
+    ));
 }
 
